e02c1f2595e2eb23d85c74968592fe15c4bcd8f9
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-06-09 20:35:33 -0700

    error page in case of invalid url

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -2,7 +2,14 @@ class UrlsController < ApplicationController
 
   def create
     shortener = Shortener.new(url_params[:long_url])
-    @url = shortener.generateShortUrl
+    @url = shortener.generateShortUrl 
+
+    if @url.persisted?
+      respond_to :js
+    else
+      render 'error.js.erb'
+    end
+
   end
 
   private 
