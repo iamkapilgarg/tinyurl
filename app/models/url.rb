38a353f6773ccd74385d829baf8da30a41c3b11b
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-06-09 20:19:41 -0700

    validating long url format

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,4 +1,15 @@
 class Url < ApplicationRecord
   validates_presence_of :short_url, :long_url
   validates_uniqueness_of :short_url
+  validate :long_url_format
+  
+  def long_url_format
+    uri = URI.parse(long_url || "")
+    if uri.host.nil?
+      errors.add(:long_url, "Invalid URL format")
+    end
+  end  
+  
 end
+
+
